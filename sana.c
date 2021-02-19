084c1adff26a1b78f7beb9e9b96bbaa33733c72b
johnk251 <jontemwass@gmail.com>
2021-02-18 17:41:03 -0900

    initial commit

---
+++ sana.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <errorn.h>
+
+int main(int arg ,char* argv[]){ 
+
+if (arg<2){
+
+printf("USAGE: cc text.txt 1 or 2  privatekey \n");
+printf("1 for encrytion and 2 for decrytion");
+
+
+}
+
+FILE * textfile ;
+FILE * outfile;
+char txt[20];
+int privatekey= atoi(argv[4]);
+int encryt= atoi(argv[3]);
+
+
+textfile=fopen(argv[1],"r");
+
+if(textfile==NULL){
+perror("Error opening file");
+
+
+}
+outfile=fopen(argv[2],"a");
+
+while(1)  {
+int c =fgetc(textfile);
+
+if(c==EOF)
+ break;
+
+
+if(encryt==1){
+c=((c+privatekey)+122)%122;
+//printf("after %c",c);
+fputc(c,outfile);
+
+}
+if(encryt==2){
+    
+
+c=((c-privatekey)+122)%122;
+
+fputc(c,outfile);
+}
+
+
+
+}
+
+
+
+
+
+
+fclose(textfile);
+fclose(outfile);
+
+
+
+}
